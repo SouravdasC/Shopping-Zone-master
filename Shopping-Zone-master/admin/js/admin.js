7eba4f7884670fcbce0c5e046aebc77f09ab9cc5
Chanchal Das <chanchaldas2221@gmail.com>
2024-10-29 18:37:38 +0530

    first commit

---
+++ Shopping-Zone-master/admin/js/admin.js
@@ -0,0 +1,50 @@
+// --- Functionality for pertial page updates ---
+const listItems = document.querySelectorAll('.controls ul li');
+const contentBoxes = document.querySelectorAll('.my-content-box');
+
+
+function hideAllContentBoxes() {
+    contentBoxes.forEach(box => {
+        box.style.display = 'none';
+    });
+}
+
+hideAllContentBoxes();
+contentBoxes[0].style.display = 'block';
+
+
+listItems.forEach((item, index) => {
+    item.addEventListener('click', function () {
+       
+        hideAllContentBoxes();
+        contentBoxes[index].style.display = 'block';
+
+        listItems.forEach(item => item.classList.remove('active'));
+        item.classList.add('active');
+    });
+});
+
+
+// --- Functionality for modal ---
+let updateBtns = document.querySelectorAll('#update-btn');
+let updateModal = document.querySelector('.update-modal');
+let closeBtn = document.querySelector('.modal-container i');
+let modalUpdateBtn = document.querySelector('#modal-update-btn');
+
+
+updateBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        updateModal.style.zIndex = '20';
+        updateModal.style.opacity = '1';
+    })
+})
+
+closeBtn.addEventListener('click', () => {
+    updateModal.style.zIndex = '-10';
+    updateModal.style.opacity = '-1';
+})
+
+modalUpdateBtn.addEventListener('click', () => {
+    updateModal.style.zIndex = '-10';
+    updateModal.style.opacity = '-1';
+})
---
+++ Shopping-Zone-master/js/profile.js
@@ -0,0 +1,55 @@
+    // --- Initializing the Variables
+    let editBtn = document.querySelectorAll('.edit-btn');
+    let saveBtn = document.querySelectorAll('.save-btn');
+
+    // --- Functionality to Edit Button 
+    editBtn.forEach((editButton) => {
+       editButton.addEventListener('click', (event) => {
+      
+        let inputField = event.target.closest('div').querySelector('input');
+        inputField.removeAttribute('readonly');
+        inputField.style.backgroundColor = 'white';
+        inputField.focus();
+        let value = inputField.value;
+        inputField.setSelectionRange(value.length, value.length);
+
+        let saveButton = event.target.closest('div').querySelector('.save-btn');
+        saveButton.style.display = 'inline-block';
+      });
+    });
+ 
+    // --- Functionality to Save Button
+    saveBtn.forEach((saveButton) => {
+      saveButton.addEventListener('click', (event) => {
+
+        let inputField = event.target.closest('div').querySelector('input');
+        inputField.setAttribute('readonly', true);
+        inputField.style.backgroundColor = 'rgb(243, 243, 243)';
+
+        let editButton = event.target.closest('div').querySelector('.edit-btn');
+        editButton.style.display = 'inline-block';
+      });
+    });
+
+    // --- Functionality Add/Edit Toggle in Email & Phone Field
+    let emailInput = document.getElementById('email');
+    let emailBtn = document.querySelector('.email-btn');
+    let phoneInput = document.getElementById('phone');
+    let phoneBtn = document.querySelector('.phone-btn');
+
+    function btnToggle() {
+      if (emailInput.value.length == 0) {
+        emailBtn.innerHTML = 'Add';
+      } else{
+        emailBtn.innerHTML = 'Edit';
+      }
+
+      if (phoneInput.value.length == 0) {
+        phoneBtn.innerHTML = 'Add';
+      } else{
+        phoneBtn.innerHTML = 'Edit';
+      }
+    }
+
+    btnToggle();
+  
